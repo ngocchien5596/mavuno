e43693dae51fbf0abf30614d870b4cd64e1bb117
ngocchien5596 <buingocchien5596@gmail.com>
2024-01-09 11:49:39 +0700

    fix chrome driver issue1

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+
 import pytest
 
 
@@ -10,9 +8,7 @@ import pytest
 def setup(browser):
     print(browser)
     if browser == 'chrome':
-        # url = 'https://python.org/'
         chrome_options = Options()
-        # chrome_options.add_experimental_option("detach", True)
         chrome_options.binary_location = '/Users/ngocchien/Downloads/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing'
         chrome_driver_path = '/Users/ngocchien/Downloads/chromedriver-mac-arm64/chromedriver'
         service_options = webdriver.ChromeService(executable_path=chrome_driver_path)
